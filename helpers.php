bc3375a43541e807f5b00bb5e571259eb77b6c8b
Aaron Lomibao <aaron.lomibao09@gmail.com>
2024-06-28 09:23:38 +0800

    UI partials and Inspect Helper Function

--- helpers.php
+++ helpers.php
@@ -9,3 +9,68 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+
+
+/**
+ * Load a view
+ * @param string $name
+ * @return void
+ *
+ */
+function loadView($name)
+{
+  $viewPath = basePath("views/{$name}.view.php");
+
+  if (!file_exists($viewPath)) {
+    echo "View '{$name} not found'";
+    return;
+  }
+
+  require $viewPath;
+}
+
+/**
+ * Load a partial
+ * @param string $name
+ * @return void
+ *
+ */
+function loadPartial($name)
+{
+
+  $partialPath = basePath("views/partials/{$name}.php");
+
+  if (!file_exists($partialPath)) {
+    echo "View '{$name} not found'";
+    return;
+  }
+
+  require $partialPath;
+}
+
+/**
+ * Inspect a value(s)
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+  die();
+}
